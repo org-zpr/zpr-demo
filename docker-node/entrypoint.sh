6f22a7522332909d72855538d3bce51c10bcb816
vvisigoth <anthony@steppe.capital>
2025-07-02 15:41:47 -0700

    clean up entrypoint file

--- docker-node/entrypoint.sh
+++ docker-node/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Add static IPv4s
-#ip addr add 129.6.7.2/32 dev eth0
 ip addr add 129.6.7.1/32 dev eth0
 
-# Ensure socket directory exists
 mkdir -p /var/run/zpr
 
 exec /app/bin/ph-no-uring node -c /authority/node-conf.toml

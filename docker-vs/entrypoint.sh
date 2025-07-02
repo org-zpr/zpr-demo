5464eb3bcc7be91379437c99f50e59871fb40569
vvisigoth <anthony@steppe.capital>
2025-07-02 15:28:41 -0700

    try to resolve ipv4 and ipv6 networking

--- docker-vs/entrypoint.sh
+++ docker-vs/entrypoint.sh
@@ -3,11 +3,12 @@
 echo "THIS IS RUNNING"
 
 # Add static IPv4s
-ip addr add 129.6.7.2/32 dev eth0
 ip addr add 129.6.7.1/32 dev eth0
 
 # Ensure socket directory exists
 mkdir -p /var/run/zpr
+
+#!/bin/sh
 
 # Wait for the IPv6 address to be assigned
 echo "⏳ Waiting for fd5a:5052::2 to appear on eth0..."
